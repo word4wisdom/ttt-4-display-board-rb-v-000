11281276324f3a20356d3e1ed4d4b136c4a08b29
Joan' Araujo <wisdombthegame@gmail.com>
2018-05-15 21:47:25 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,5 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+board = [" "," ", " ", " "," "," "," "," "," ",]
+
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -6,4 +7,5 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+end   
+
